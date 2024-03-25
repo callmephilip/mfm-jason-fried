77a792b48558d957ec1dc6b830d0c26519db375f
Philip Nuzhnyi <philip.nuzhnyy@gmail.com>
2024-03-25 23:42:48 +0000

    setup

---
+++ README.md
@@ -0,0 +1,9 @@
+## Hooks Over Towel Bars: Jason Fried's Philosophy on Product Design - made with Lumentis
+
+`curl -fsSL https://bun.sh/install | bash # Install bun for macOS, Linux, and WSL`
+
+`bun install`
+
+`bun dev`
+
+Change things in `pages` to see the effect.
---
+++ theme.config.jsx
@@ -0,0 +1,31 @@
+export default {
+      logo: <span>Hooks Over Towel Bars: Jason Fried's Philosophy on Product Design - made with Lumentis</span>,
+      editLink: {
+        component: null,
+      },
+      project: {
+        link: "https://github.com/hrishioa/lumentis",
+      },
+      feedback: {
+        content: null,
+      },
+      footer: {
+        text: (
+          <>
+            Made with 🫶 by&nbsp;
+            <a href="https://twitter.com/hrishioa" target="_blank">
+              Hrishi - say hi!
+            </a>
+          </>
+        ),
+      },
+      head: (
+        <>
+          <meta property="og:title" content="Hooks Over Towel Bars: Jason Fried's Philosophy on Product Design" />
+          <meta property="og:description" content="Podcast with 2 hosts (Sam and Shaan) and their guest Jason Fried" />
+          <meta name="robots" content="noindex, nofollow" />
+          <link rel="icon" type="image/x-icon" href="https://raw.githubusercontent.com/HebeHH/lumentis/choose-favicon/assets/default-favicon.png" />
+        </>
+      ),
+    };
+    
